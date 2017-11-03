9836fb16344704c1d652bdfac559ffc05b6795b6
Kylir <Kylir@users.noreply.github.com>
2017-11-03 17:59:59 +0000

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# oled-text
-NodeJS micro service to display text on an oled screen on the raspberry pi.
+# Oled Text
+
+A NodeJS micro service to display text on an I2C oled screen connected to a Raspberry Pi.
+
+## Inspirations
+
++ [Running your application in a docker container.](https://nodejs.org/en/docs/guides/nodejs-docker-webapp/)
++ [Create a NodeJS micro service.](https://mxstbr.blog/2017/01/your-first-node-microservice/)
---
+++ index.js
@@ -0,0 +1,41 @@
+/* jshint undef: true, node: true, esnext: true, asi: true */
+
+'use strict'
+
+const {send, json} = require('micro'),
+    Oled = require('oled-i2c-bus'),
+    i2c = require('i2c-bus'),
+    c = require('../config/constants')
+
+let i2cBus = i2c.openSync(1)
+
+let opts = {
+    width: c.WIDTH,
+    height: c.HEIGHT,
+    address: c.I2C_ADDR
+}
+
+let oled = new Oled(i2cBus, opts)
+oled.clearDisplay()
+oled.turnOnDisplay()
+
+module.exports = async (req, res) => {
+    const body = await json(req)
+
+    let text = body.text,
+        status = 200,
+        data = {}
+
+    if (!text) {
+        status = 400
+        data.msg = 'Bad Request. No text property specified in the query.'
+    } else {
+        //(re)set the cursor and display the text
+        oled.setCursor(1, 1);
+        oled.writeString(font, 1, text, 1, true);
+        data.msg = 'Ok. Text sent to the screen.'
+    }
+
+    send(res, status, data)
+
+}
